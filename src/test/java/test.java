53887abae9defa885d21c72a30378225c45d5d07
Ram Santhosh <ramu3317@gmail.com>
2025-03-20 07:56:33 +0530

    modified from 2nd brach

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -4,6 +4,16 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("gitx");
+		
+		System.out.print("rammmm");
+
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("gi22tx");
+		
+		System.out.print("2");
 
 	}
 
